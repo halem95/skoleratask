1f7a2a28458bc3c9d8a25697b0ca26b85cbc55a0
muhammed elsayed <mohammed272041@science.tanta.edu.eg>
2018-10-06 23:21:38 +0200

    first ver of ui task

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('.nav-link').click(function () {
+        $('.nav-link').removeClass('opendropdown');
+        $(this).addClass('opendropdown');
+    });
+
+    $('.dropdown-item').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        
+    });
+
+    $('.tab-item').click(function () {
+        $('.tab-item').removeClass('active-tab-item');
+        $(this).addClass('active-tab-item');
+    });
+
+    var spanHeight = $('.register-step-border').parents('.subject-register-steps').find('.register-step-number').outerHeight();
+    var navHeight = $('.navbar-parent').outerHeight();
+    var loadingBarHeight = $('.gradient').innerHeight();
+
+    $('.register-step-border').css('top', (spanHeight / 2));
+
+    $('.navbar-collapse').css('top', (navHeight + loadingBarHeight ));
+
+    $('.register-step-number').click(function() {
+        $('.register-step-number').removeClass('active-register-step');
+        $(this).addClass('active-register-step');
+    })
+})
